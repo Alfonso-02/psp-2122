f4050bc476eff13746836f00bf58c005e6afbc06
Alfonso-02 <al407.com@gmail.com>
2021-11-09 19:13:04 +0100

    HilosEjemplo2

---
+++ Ejercicios/src/Hilos/HilosEjemplo2.java
@@ -0,0 +1,23 @@
+package Hilos;
+
+class HiloEjemplo2 extends Thread {
+    public void run() {
+        System.out.println("Dentro del Hilo:" + this.getName () +
+                ", Prioridad: " + this.getPriority() +
+                ", ID: " + this.getId() );
+    }
+
+    public static void main(String[] args) {
+        HiloEjemplo2 h = null;
+        for (int i = 0; i < 3; i++) {
+            h = new HiloEjemplo2(); //crear hilo
+            h.setName("HIL0" + i); //damos nombre al hilo
+            h.setPriority(i + 1); //damos prioridad
+            h.start(); //iniciar hilo
+            System.out.println("Información del " +
+                    h.getName() + ": " + h.toString());
+
+        }
+            System.out.println("3 HILOS CREADOS...");
+        }
+    }
